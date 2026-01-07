c5c2f77c6b171171424624ad1300023e2b16e321
shaton <ningchengningchuan@gmail.com>
2026-01-07 17:56:58 +0800

    Site updated: 2026-01-07 17:56:50

--- js/aplayer.js
+++ js/aplayer.js
@@ -82,11 +82,6 @@
           artist: '王羚柔',
           url: 'https://music.163.com/song/media/outer/url?id=301107.mp3',
           cover: '/image/post/girl.jpg'
-        }, {
-          name: '不想',
-          artist: '王羚柔',
-          url: 'https://music.163.com/song/media/outer/url?id=301094.mp3',
-          cover: '/image/post/girl.jpg'
         }
       ]
     });
